6b75eab8e15141f7c17882f27adfe99ade19824a
Flavio Wuensche <f.wuensche@gmail.com>
2022-11-26 11:58:43 +0100

    extract api base url

--- bin/cherry.js
+++ bin/cherry.js
@@ -5,6 +5,8 @@ import { program } from 'commander'
 import { findOccurrences } from '../src/occurrences.js'
 import { configuration } from '../src/configuration.js'
 
+const API_BASE_URL = 'http://localhost:3000/api' // TODO: convert to production url
+
 program.command('run').action((options) => {
   const occurrences = findOccurrences(configuration)
   console.log(occurrences)
@@ -14,7 +16,7 @@ program.command('push').action((options) => {
   const occurrences = findOccurrences(configuration)
   console.log(`Uploading ${occurrences.length} occurrences...`)
   axios
-    .post('http://localhost:3000/api/occurrences.json', { occurrences: JSON.stringify(occurrences) })
+    .post(API_BASE_URL + '/occurrences', { occurrences: JSON.stringify(occurrences) })
     .then(({ data }) => console.log('Response:', data))
     .catch((error) => console.error(error.message))
 })
