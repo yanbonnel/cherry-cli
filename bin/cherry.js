2d4733737cd8b2b2c0be2d9566dfd3270ee0657a
Romain Choquet <romain.choquet@gmail.com>
2022-11-27 17:50:18 +0100

    cherry init

--- bin/cherry.js
+++ bin/cherry.js
@@ -3,18 +3,37 @@
 import axios from 'axios'
 import { program } from 'commander'
 import { findOccurrences } from '../src/occurrences.js'
-import { configuration } from '../src/configuration.js'
+import { configurationExists, getConfiguration, createConfigurationFile } from '../src/configuration.js'
+import prompt from 'prompt'
+import { guessRepoName } from '../src/git.js'
 
 const API_BASE_URL = 'https://www.cherrypush.com/api'
 
-program.command('run').action((options) => {
+program.command('init').action(async () => {
+  if (configurationExists()) {
+    console.error('.cherry.js already exists, run `cherry run` instead')
+    process.exit(0)
+  }
+
+  prompt.message = 'Enter'
+  prompt.start()
+  const { name } = await prompt.get({
+    properties: { name: { description: 'Project name', default: guessRepoName(), required: true } },
+  })
+  createConfigurationFile(name)
+  console.log('.cherry.js file successfully created! You can now run `cherry run` to test it')
+})
+
+program.command('run').action(async () => {
+  const configuration = await getConfiguration()
   const occurrences = findOccurrences(configuration)
   console.log(occurrences)
   console.log(`There are ${occurrences.length} occurrences ready to be reported.`)
   console.log('Run `cherry push` to push them to your public dashboard.')
 })
 
-program.command('push').action((options) => {
+program.command('push').action(async () => {
+  const configuration = await getConfiguration()
   const occurrences = findOccurrences(configuration)
   console.log(`Uploading ${occurrences.length} occurrences...`)
   axios
--- src/occurrences.js
+++ src/occurrences.js
@@ -1,4 +1,3 @@
-import { getRepo, getMetrics } from './configuration.js'
 import { readlines } from './readlines.js'
 import Codeowners from './owners.js'
 import * as git from './git.js'
@@ -7,25 +6,30 @@ const owners = new Codeowners()
 
 export const findOccurrences = (configuration) => {
   const occurrences = []
-  const metrics = getMetrics(configuration)
 
   const sha = git.sha()
   git.files().forEach((filePath) => {
-    metrics.forEach(({ name, pattern }) => {
-      readlines(filePath, (line, lineNumber) => {
-        if (!line.match(pattern)) return
+    try {
+      configuration.metrics.forEach(({ name, pattern }) => {
+        readlines(filePath, (line, lineNumber) => {
+          if (!line.match(pattern)) return
 
-        occurrences.push({
-          commit_sha: sha,
-          file_path: filePath,
-          line_number: lineNumber,
-          line_content: line.trim().slice(0, 120).replace(/\0/, ''),
-          repo: getRepo(configuration),
-          owners: owners.getOwner(filePath),
-          metric_name: name,
+          occurrences.push({
+            commit_sha: sha,
+            file_path: filePath,
+            line_number: lineNumber,
+            line_content: line.trim().slice(0, 120).replace(/\0/, ''),
+            repo: configuration.repo,
+            owners: owners.getOwner(filePath),
+            metric_name: name,
+          })
         })
       })
-    })
+    } catch (error) {
+      if (error.code === 'ENOENT') return // TODO: understand why a file could not exist
+      if (error.code === 'EISDIR') return
+      throw error
+    }
   })
 
   return occurrences
